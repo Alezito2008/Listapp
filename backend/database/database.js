810a793324c326ae9114e46180792dd9e71b35ef
Alezito2008 <none>
2024-10-28 10:10:53 -0300

    Creado modelo Amigos

--- backend/database/database.js
+++ backend/database/database.js
@@ -2,7 +2,6 @@ const { Sequelize } = require('sequelize')
 
 require('dotenv').config()
 
-console.log(process.env.MYSQL_PASSWORD)
 const sequelize = new Sequelize('Listapp', 'root', process.env.MYSQL_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
